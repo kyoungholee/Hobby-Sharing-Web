9adf85f8fabe9767919579d9eb38c9d826ac1873
이경호 <dlrudgh1303@gmail.com>
2021-10-20 16:48:06 +0900

    웹 페이지 메뉴부분 카테고리 추가

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import React from "react";
 import Header from "./component/Header";
+import Category from "./component/Category";
 import Main from "./component/Main";
 import Footer from "./component/Footer";
+import Login from "./component/User/Login";
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+
 
 function App() {
   return (
     <>
-      <div className="App">
-        <Header />
-        <Main />
-        <Footer />
-        
-      </div>
+      <BrowserRouter>    
+        <div className="App">
+          <Header />
+          <Category />
+
+          <Switch>
+      
+          <Route path = "/">
+          <Main />
+          </Route>
+          </Switch>
+
+          
+          <Footer />
+          
+        </div>
+      </BrowserRouter>
     </>
   );
 }
