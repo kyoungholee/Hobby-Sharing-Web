33e0dd4b5856af2867838e639b6673ba3f410e5f
이경호 <dlrudgh1303@gmail.com>
2021-10-25 02:18:57 +0900

    메인 페이지 수정

--- src/Pages/SmOutSideList.js
+++ src/Pages/SmOutSideList.js
@@ -1,18 +1,30 @@
-import React from 'react'
+import React, { useState }  from 'react'
 import './SmOutSideList.css';
 import dummy from "../DB/Summer.json";
 
+
 function SmOutSideList() {
+
+    const [number, setNumber] = useState(0);
+    
+    const onIncrease = () => {
+        setNumber(number + 1);
+    }
+
+
     return (
         <>    
             <div className = "outsideList">
                 <ul>
                     {dummy.summber.map(list => (
                         <li key = {list.id}>
+                        <div className = "images">
                         <img src = {list.image} alt = {list.name} width = "260px" height = "340px"/>
-
+                        </div>
                             <div className = "name"> {list.name} </div>
-                        <div className = "explan">{list.explanation}</div>
+                        <div className = "explan">{list.explanation}
+                            <button onClick = {onIncrease}>👍{number}</button>
+                        </div>
                         <div className = "price">{list.price}</div>
 
                         </li>
--- src/Pages/WnOutSideList.js
+++ src/Pages/WnOutSideList.js
@@ -9,7 +9,9 @@ function WnOutSideList() {
                 <ul>
                 {dummy.Winnter.map(list => (
                     <li key = {list.id}>
+                    <div className = "images">
                     <img src = {list.image} alt = {list.name} width = "250x" height = "300px"/>
+                    </div>
 
                         <div className = "name"> {list.name} </div>
                     <div className = "explan">{list.explanation}</div>
