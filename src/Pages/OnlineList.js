e238b0a0038f0e9e933d426aaef399dbe3c63fa5
이경호 <dlrudgh1303@gmail.com>
2021-10-29 00:40:33 +0900

    메인 페이지 배너 광고 슬라이스 삽입

--- src/Pages/OnlineList.js
+++ src/Pages/OnlineList.js
@@ -1,37 +1,55 @@
-import React from 'react'
+import React, {useRef} from 'react';
 import './OnlineList.css';
 import dummy from '../DB/Online.json';
 import { Swiper , SwiperSlide} from "swiper/react";
-import SwiperCore, { 
-    Navigation, 
-    Pagination, 
-  } from "swiper"; //*
-
-  import {ArrowLeftOutlined, ArrowRightOutlined} from "@ant-design/icons";
-
-import "../Swiper/swiper.scss";
-import "../Swiper/Navigation.scss";
-
+import SwiperCore, { Navigation, Pagination} from "swiper"; 
+import left from '../images/left 2.JPG';
+import right from '../images/right.JPG';
 
 function OnlineList() {
+
+
+    const prevRef = useRef(null)
+    const nextRef = useRef(null)
+
     return (
         <>
             <div className = "OnlineList">
-                <Swiper className = "Swiper">
+                <Swiper className = "Swiper"
+
+
+                navigation={{
+                    prevEl: prevRef.current ? prevRef.current : undefined,
+                    nextEl: nextRef.current ? nextRef.current : undefined,
+                  }}
+
+            
+                  spaceBetween={50}
+                  slidesPerView={1}
+                  navigation ={{ clickable: true }}
+                  pagination={{ clickable: true }}
+                  autoplay={{ delay: 1000 }}
+                >
                     {dummy.online.map(list => (
 
                     <SwiperSlide key = {list.id} className = "swiper-slide">
-                        <div className = "arrow">
-                            <ArrowLeftOutlined  className = "Left" />
-                            <div className = "name">{list.name}</div>
-                            <ArrowRightOutlined  className = "right"/>
-                        </div>       
+
+                        <div className = "images">
+                        <img src = {list.image} alt = {list.name} width = "400px" height = "320px"/>
+                        </div>
+
                     </SwiperSlide>
                         ))}
+                        <div ref={prevRef} className = "left">
+                            <img src = {left} alt = "left" width = "44px" height = "32px" />
+                        </div>
+                        <div ref={nextRef} className = "right">
+                            <img src = {right} alt = "right" width = "44px" height = "32px" />
+                        </div>
                 </Swiper>
             </div>
         </>
-    )
+    ) 
 
 }
 
--- src/component/Main.js
+++ src/component/Main.js
@@ -12,6 +12,10 @@ function Main() {
             <div>
                 <OnlineList />
             </div>
+
+            <h2> 온라인 배너가 6개를 넣고 한장씩 넘어가는 형태</h2>
+
+
             <h1 className = "summber_title">여름 특가!! 여름에 추천하는 취미</h1>
                 <SmOutSideList />
             <h1 className = "winnter-title"> 겨울 특가!! 겨울에 추천하는 취미</h1>
